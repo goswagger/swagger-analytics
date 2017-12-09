ef1f9e3e8ed41f782b62ceb06de56e74d87e045b
notacharade <teamgoswagger@gmail.com>
2017-12-09 14:46:50 -0800

    add push to webserver

--- processlist.sql
+++ processlist.sql
@@ -1,1 +1,7 @@
-select CONCAT("./goswagger.sh ",athlete.id, " ", access_token, " |& tee ", athlete.id, ".out") from members;
+select CONCAT("./goswagger.sh ",athlete.id, " ", access_token, " |& tee ", athlete.id, ".out") from members
+UNION
+select CONCAT('curl --data-urlencode "data@analysis01-',athlete.id,'.out" --data-urlencode "tableid=analysis01-',athlete.id,'.out" http://www.goswagger.com/sample-page/?reqtype=updanalysis') from members
+UNION
+select CONCAT('curl --data-urlencode "data@analysis02-',athlete.id,'.out" --data-urlencode "tableid=analysis02-',athlete.id,'.out" http://www.goswagger.com/sample-page/?reqtype=updanalysis') from members
+UNION
+select CONCAT('curl --data-urlencode "data@analysis03-',athlete.id,'.out" --data-urlencode "tableid=analysis03-',athlete.id,'.out" http://www.goswagger.com/sample-page/?reqtype=updanalysis') from members;
