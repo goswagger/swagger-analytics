b22653a7c4689608cbf8abe0bd3a0b8ccffd8541
notacharade <teamgoswagger@gmail.com>
2017-12-10 05:25:57 +0000

    delete 7 lines at top in sed, rather than just one since we have union in select and beeline adds more output to remove.

--- getprocesslist.sh
+++ getprocesslist.sh
@@ -1,3 +1,3 @@
 beeline -u jdbc:hive2://hiveserver:10000/default --showHeader=false --slient=true --outputformat=tsv2 -n hive -p hive -f processlist.sql > processlist.sh
-sed -i '1,1d;$ d' processlist.sh
+sed -i '1,7d;$ d' processlist.sh
 chmod 777 processlist.sh
