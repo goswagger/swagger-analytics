29039e8dff590d82658894d46af0b9cf52dd9ca2
notacharade <teamgoswagger@gmail.com>
2017-12-10 17:48:58 +0000

    leaderboard processing needs to be done outside script but keep it here for now

--- getprocesslist.sh
+++ getprocesslist.sh
@@ -1,5 +1,5 @@
-# drop and re-create hive tables
-beeline -u jdbc:hive2://hiveserver:10000/default -n hive -p hive -f swaggercreatehivetables2.sql
+# drop and re-create hive table for leaderboard (done once per processing cycle)
+# beeline -u jdbc:hive2://hiveserver:10000/default -n hive -p hive -f swaggercreatehivetables2.sql
 # create list of commands to execute for each athlete in members table
 beeline -u jdbc:hive2://hiveserver:10000/default --showHeader=false --slient=true --outputformat=tsv2 -n hive -p hive -f processlist.sql > processlist.sh
 sed -i '1,7d;$ d' processlist.sh
