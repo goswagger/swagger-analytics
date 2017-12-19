a5da8e4399d199f8bffcff0cb783eec4aa471a7a
notacharade <teamgoswagger@gmail.com>
2017-12-18 16:27:57 -0800

    add hyperlink to athlete, remove athleteid from display, add ranking to display

--- swaggerking.sql
+++ swaggerking.sql
@@ -1,4 +1,4 @@
-select athlete_id, max(athlete_name) as athlete, 
+select ROW_NUMBER() OVER () AS rank, athlete_name as athlete, 
 sum(case rank
 when 1 then 25
 when 2 then 18
@@ -13,39 +13,39 @@ when 10 then 1
 else 0
 end) as points
 from (
-select b.entries[0].athlete_id AS athlete_id, b.entries[0].athlete_name as athlete_name, b.entries[0].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[0].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[0].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[0].rank as rank
 from leaderboardforsegment b
 union
-select b.entries[1].athlete_id as athlete_id, b.entries[1].athlete_name as athlete_name, b.entries[1].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[1].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[1].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[1].rank as rank
 from leaderboardforsegment b
 union
-select b.entries[2].athlete_id as athlete_id, b.entries[2].athlete_name as athlete_name, b.entries[2].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[2].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[2].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[2].rank as rank
 from leaderboardforsegment b
 union
-select b.entries[3].athlete_id as athlete_id, b.entries[3].athlete_name as athlete_name, b.entries[3].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[3].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[3].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[3].rank as rank
 from leaderboardforsegment b
 union
-select b.entries[4].athlete_id as athlete_id, b.entries[4].athlete_name as athlete_name, b.entries[4].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[4].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[4].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[4].rank as rank
 from leaderboardforsegment b
 union
-select b.entries[5].athlete_id as athlete_id, b.entries[5].athlete_name as athlete_name, b.entries[5].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[5].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[5].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[5].rank as rank
 from leaderboardforsegment b
 union
-select b.entries[6].athlete_id as athlete_id, b.entries[6].athlete_name as athlete_name, b.entries[6].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[6].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[6].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[6].rank as rank
 from leaderboardforsegment b
 union
-select b.entries[7].athlete_id as athlete_id, b.entries[7].athlete_name as athlete_name, b.entries[7].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[7].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[7].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[7].rank as rank
 from leaderboardforsegment b
 union
-select b.entries[8].athlete_id as athlete_id, b.entries[8].athlete_name as athlete_name, b.entries[8].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[8].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[8].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[8].rank as rank
 from leaderboardforsegment b
 union
-select b.entries[9].athlete_id as athlete_id, b.entries[9].athlete_name as athlete_name, b.entries[9].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[9].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[9].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[9].rank as rank
 from leaderboardforsegment b
 union
-select b.entries[10].athlete_id as athlete_id, b.entries[10].athlete_name as athlete_name, b.entries[10].rank as rank, b.segment_id as segment_id
+select CONCAT("<a href='https://www.strava.com/athletes/",b.entries[10].athlete_id,"/' rel='noopener noreferrer' target='_blank'>",regexp_replace(b.entries[10].athlete_name, '[^\\x20-\\x7A]+', ''),'</a>') as athlete_name, b.entries[10].rank as rank
 from leaderboardforsegment b
 ) as t2
-group by athlete_id, athlete_name
+group by athlete_name
 having points > 0
 order by points desc;
