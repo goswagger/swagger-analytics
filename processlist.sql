53748a9a58fa2f334ce1038d38177bde5fdf1e5e
notacharade <teamgoswagger@gmail.com>
2017-12-20 19:58:23 -0800

    add swaggerking

--- processlist.sql
+++ processlist.sql
@@ -5,3 +5,5 @@ UNION
 select CONCAT('curl --data-urlencode "data@analysis02-',athlete.id,'.out" --data-urlencode "tableid=analysis02-',athlete.id,'.out" http://www.goswagger.com/sample-page/?reqtype=updanalysis') from members
 UNION
 select CONCAT('curl --data-urlencode "data@analysis03-',athlete.id,'.out" --data-urlencode "tableid=analysis03-',athlete.id,'.out" http://www.goswagger.com/sample-page/?reqtype=updanalysis') from members;
+UNION
+select CONCAT('curl --data-urlencode "data@swaggerking-',athlete.id,'.out" --data-urlencode "tableid=swaggerking-',athlete.id,'.out" http://www.goswagger.com/sample-page/?reqtype=updanalysis') from members;
